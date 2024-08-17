8b8b568130abae87d7c5f2e15826971612ccff2a
Narai Risser (RIT Student) <ngr7989@rit.edu>
2024-08-17 12:51:50 -0400

    Planet prefabs

--- Code/Planet.gd
+++ Code/Planet.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-@export var shipParent : Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,5 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print_debug(shipParent.vel)
-	look_at(global_position + shipParent.vel)
+	pass
